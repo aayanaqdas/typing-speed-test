680c7e05810f471fd378868eb3b133a68126fcf0
aayanaqdas <aayan.aqdas@outlook.com>
2026-01-25 03:17:17 +0100

    mobile layout finished and added the input + text area

--- src/main.js
+++ src/main.js
@@ -1,4 +1,7 @@
 const dropDownBtns = document.querySelectorAll(".dropdown-btn");
+const typingInput = document.getElementById("typing-input");
+const quoteDisplay = document.getElementById("quote-display");
+
 dropDownBtns.forEach((btn) => {
   const dropDownContent = btn.nextElementSibling;
   const dropDownInputs = dropDownContent.querySelectorAll(`input[type="radio"]`);
@@ -24,3 +27,11 @@ dropDownBtns.forEach((btn) => {
     }
   });
 });
+
+typingInput.addEventListener("input", () => {
+  console.log(typingInput.value);
+});
+
+const quote = document.createElement("span");
+quote.textContent = `The archaeological expedition unearthed artifacts that complicated prevailing theories about Bronze Age trade networks. Obsidian from Anatolia, lapis lazuli from Afghanistan, and amber from the Baltic—all discovered in a single Mycenaean tomb—suggested commercial connections far more extensive than previously hypothesized. \"We've underestimated ancient peoples' navigational capabilities and their appetite for luxury goods,\" the lead researcher observed. \"Globalization isn't as modern as we assume.\"`;
+quoteDisplay.appendChild(quote);
